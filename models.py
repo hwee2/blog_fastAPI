4224a183d19301cb78f6c73de7ad389d31c13f28
hwee <gee6121@naver.com>
2026-06-23 19:48:38 +0900

    feat: comment 모델 생성

--- models.py
+++ models.py
@@ -1,6 +1,10 @@
-from sqlalchemy import Integer, String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from xml.dom.minidom import Comment
+
+from sqlalchemy import Integer, String, ForeignKey, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from database.orm import Base
+from datetime import datetime
+
 
 # Article 모델 정의
 class Article(Base):
@@ -12,3 +16,15 @@ class Article(Base):
 
     content : Mapped[String] = mapped_column(String(1000))
 
+    comments: Mapped[list["Comment"]] = relationship(back_populates="article", cascade="all, delete-orphan")
+
+#Comment 모델
+class Comments(Base):
+    __tablename__ = 'comment'
+
+    id : Mapped[int] = mapped_column(Integer, primary_key=True,autoincrement=True)
+    author: Mapped[String] = mapped_column(String(20))
+    content: Mapped[String] = mapped_column(String(200))
+    created_at : Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+    article_id : Mapped[Article] = mapped_column(ForeignKey("article.id"), nullable=False)
+    article: Mapped[Article] = relationship(back_populates="comments")
